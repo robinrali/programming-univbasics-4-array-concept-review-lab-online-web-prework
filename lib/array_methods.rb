250005e808431b06446305b904eb74c40c20f0f2
rabab raza ali <robin.ali85@yahoo.com>
2019-07-12 15:45:23 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,21 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+def find_element_index (array, value_to_find)
+  # check if value_to_find is in array
+  # if so, return the index where it is found
+  # otherwise, return nil if value is not found
+  
+  counter = 0
+  while array[counter] do
+  counter += 1
+  if value not found, return nil
 end
+  
+end 
 
-def find_max_value(array)
-  # Add your solution here
+def find_index
+
+ ([1,2,1,3,4,3,5,4,3,2,1])
 end
 
 def find_min_value(array)
-  # Add your solution here
+([10,5,3,7,1,9,1,3,10])
 end
